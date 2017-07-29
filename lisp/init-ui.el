5b90d6281bdd746d489a5c537e02aca69f40cb5b
Yiming Chen <dsdshcym@gmail.com>
2017-07-29 15:18:19 +0800

    set font to Iosevka and PingFang SC

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -4,6 +4,17 @@
 (menu-bar-mode -1)
 (tooltip-mode -1)
 
+;; copy/pasted from spacemacs chinese layer
+(defun private/set-monospaced-font (english chinese english-size chinese-size)
+  (set-face-attribute 'default nil :font
+                      (format "%s:pixelsize=%d" english english-size))
+  (set-frame-font english)
+  (dolist (charset '(kana han cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font) charset
+                      (font-spec :family chinese :size chinese-size))))
+
+(private/set-monospaced-font "Iosevka" "PingFang SC" 14 14)
+
 (use-package solarized-theme
   :ensure t
   :init (progn
