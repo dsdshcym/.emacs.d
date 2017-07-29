5437c35e6b9d347d9fe78139eb1c2684b725ae20
Yiming Chen <dsdshcym@gmail.com>
2017-07-29 15:13:23 +0800

    ui: use solarized-dark theme

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -4,4 +4,11 @@
 (menu-bar-mode -1)
 (tooltip-mode -1)
 
+(use-package solarized-theme
+  :ensure t
+  :init (progn
+          (setq solarized-use-variable-pitch nil)
+          (setq solarized-scale-org-headlines nil)
+          (load-theme 'solarized-dark t nil)))
+
 (provide 'init-ui)
