65c945bf0f8e4c6e8c235d7c0ee51d0cd5b19b75
Yiming Chen <dsdshcym@gmail.com>
2017-07-30 14:16:59 +0800

    add pangu-spacing mode

--- lisp/init-chinese.el
+++ lisp/init-chinese.el
@@ -2,4 +2,11 @@
   :init
   (fcitx-evil-turn-on))
 
+(use-package pangu-spacing
+  :defer t
+  :init
+  (progn
+    (setq pangu-spacing-real-insert-separtor t)
+    (global-pangu-spacing-mode 1)))
+
 (provide 'init-chinese)
