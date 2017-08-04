6b53be30b5f046f19fba3f7e95d10da98c1eafd9
Yiming Chen <dsdshcym@gmail.com>
2017-08-04 17:35:56 +0800

    mac: use osx-dictionary

--- lisp/init-mac.el
+++ lisp/init-mac.el
@@ -8,4 +8,21 @@
    "dd" 'dash-at-point
    "dD" 'dash-at-point-with-docset))
 
+(use-package osx-dictionary
+  :commands (osx-dictionary-search-pointer
+             osx-dictionary-search-input
+             osx-dictionary-cli-find-or-recompile)
+  :general
+  (general-define-key
+   :prefix "SPC"
+   :non-normal-prefix "M-m"
+   :keymaps '(motion insert emacs)
+   "xd" 'osx-dictionary-search-pointer
+   "xD" 'osx-dictionary-search-input)
+  (general-evil-define-key 'normal 'osx-dictionary-mode-map
+   "q" 'osx-dictionary-quit
+   "r" 'osx-dictionary-read-word
+   "s" 'osx-dictionary-search-input
+   "o" 'osx-dictionary-open-dictionary.app))
+
 (provide 'init-mac)
