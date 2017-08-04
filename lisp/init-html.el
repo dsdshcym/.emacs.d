5369cb4a00679ab44dc9a4f08a823be1997b363b
Yiming Chen <dsdshcym@gmail.com>
2017-08-04 17:48:15 +0800

    html: use company-css

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,6 +1,14 @@
 (use-package slim-mode
   :defer t)
 
+(use-package company-css
+  :defer t
+  :init
+  (add-hook 'css-mode-hook
+              (lambda ()
+                (make-local-variable 'company-backends)
+                (add-to-list 'company-backends company-css))))
+
 (use-package company-web
   :defer t
   :init
