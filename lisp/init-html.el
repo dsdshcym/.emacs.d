376160025bae83509456ef1033c12bf4f56fc5ca
Yiming Chen <dsdshcym@gmail.com>
2017-08-04 17:47:08 +0800

    html: use company-web

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,4 +1,17 @@
 (use-package slim-mode
   :defer t)
 
+(use-package company-web
+  :defer t
+  :init
+  (progn
+    (add-hook 'slim-mode-hook
+              (lambda ()
+                (make-local-variable 'company-backends)
+                (add-to-list 'company-backends 'company-web-slim)))
+    (add-hook 'html-mode-hook
+              (lambda ()
+                (make-local-variable 'company-backends)
+                (add-to-list 'company-backends 'company-web-html)))))
+
 (provide 'init-html)
