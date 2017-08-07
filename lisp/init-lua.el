91716dec75bc2e66a5561b788d789771e97bd7df
Yiming Chen <dsdshcym@gmail.com>
2017-08-07 15:07:26 +0800

    lua: use company-lua for auto completion in lua

--- lisp/init-lua.el
+++ lisp/init-lua.el
@@ -5,4 +5,12 @@
   :init
   (setq lua-indent-level 2))
 
+(use-package company-lua
+  :defer t
+  :init
+  (add-hook 'lua-mode-hook
+            (lambda ()
+              (make-local-variable 'company-backends)
+              (add-to-list 'company-backends 'company-lua))))
+
 (provide 'init-lua)
