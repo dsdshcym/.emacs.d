2ba2c3574f7c59a28e51bbbc45fed632977fef9c
Yiming Chen <dsdshcym@gmail.com>
2017-08-09 22:28:02 +0800

    org: strip whitespaces between CJK characters when export to html

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -244,6 +244,19 @@
     (add-to-list 'org-latex-packages-alist '("" "listings"))
     (add-to-list 'org-latex-packages-alist '("" "color"))
 
+    ;; copy/pasted from spacemacs chinese layer
+    (defadvice org-html-paragraph (before org-html-paragraph-advice
+                                          (paragraph contents info) activate)
+      "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to html."
+      (let* ((origin-contents (ad-get-arg 1))
+             (fix-regexp "[[:multibyte:]]")
+             (fixed-contents
+              (replace-regexp-in-string
+               (concat
+                "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+        (ad-set-arg 1 fixed-contents)))
+
     (setq org-html-mathjax-options
           (quote
            ((path "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG")
