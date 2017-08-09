93429bce1eacc740cedd1128a17de184d058f58e
Yiming Chen <dsdshcym@gmail.com>
2017-08-09 20:27:58 +0800

    keybinding: use general-define-key to define-key

--- lisp/init-keybinding.el
+++ lisp/init-keybinding.el
@@ -49,19 +49,7 @@ By default the (truly) last line."
           (goto-char (buffer-size))
         (goto-char (point-min))
         (forward-line (1- count)))
-      (evil-first-non-blank))
-
-    ;; esc should always quit: http://stackoverflow.com/a/10166400/61435
-    (define-key evil-normal-state-map [escape] 'keyboard-quit)
-    (define-key evil-visual-state-map [escape] 'keyboard-quit)
-    (define-key minibuffer-local-map [escape] 'abort-recursive-edit)
-    (define-key minibuffer-local-ns-map [escape] 'abort-recursive-edit)
-    (define-key minibuffer-local-completion-map [escape] 'abort-recursive-edit)
-    (define-key minibuffer-local-must-match-map [escape] 'abort-recursive-edit)
-    (define-key minibuffer-local-isearch-map [escape] 'abort-recursive-edit)
-    ;; Escape from isearch-mode("/" and "?" in evil-mode) like vim
-    (define-key isearch-mode-map (kbd "<escape>") 'isearch-cancel)
-    (define-key evil-motion-state-map [C-i] 'evil-jump-forward)))
+      (evil-first-non-blank))))
 
 (use-package general
   :config
@@ -143,9 +131,26 @@ By default the (truly) last line."
      :keymaps '(info-mode-map compilation-mode-map))
 
     (general-define-key
+     :keymaps '(minibuffer-local-map
+                minibuffer-local-ns-map
+                minibuffer-local-completion-map
+                minibuffer-local-must-match-map
+                minibuffer-local-isearch-map)
+     [escape] 'abort-recursive-edit)
+
+    ;; Escape from isearch-mode("/" and "?" in evil-mode) like vim
+    (general-define-key
+     :keymaps 'isearch-mode-map
+     [escape] 'isearch-cancel)
+
+    (general-define-key
      :keymaps 'input-decode-map
      "C-h" "DEL"
-     "C-i" [C-i])))
+     "C-i" [C-i])
+
+    (general-define-key
+     :keymaps 'motion
+     "C-i" 'evil-jump-forward)))
 
 (use-package evil-nerd-commenter
   :defer t
