733e0a9dbdf5e6356acb4d3e94e1a6bbe1f691bd
Yiming Chen <dsdshcym@gmail.com>
2017-08-09 18:04:31 +0800

    keybinding: fix init-keybinding.el indentations

--- lisp/init-keybinding.el
+++ lisp/init-keybinding.el
@@ -1,141 +1,144 @@
 (use-package evil
-  :init (progn
-          (setq evil-want-Y-yank-to-eol t)
-          (setq evil-symbol-word-search t)
-          (setq evil-want-C-u-scroll t)
-          (evil-mode))
-  :config (progn
-            (mapc #'evil-declare-ignore-repeat
-                  '(kill-this-buffer
-                    ido-kill-buffer
-                    outline-next-visible-heading
-                    outline-previous-visible-heading
-                    outline-up-heading
-                    evil-visualstar/begin-search-forward
-                    evil-visualstar/begin-search-backward
-                    org-export-dispatch
-                    org-end-of-line
-                    org-beginning-of-line
-                    org-open-at-point
-                    org-cycle
-                    org-shifttab
-                    org-ctrl-c-ctrl-c
-                    org-next-visible-heading
-                    org-previous-visible-heading
-                    split-window-below
-                    split-window-below-and-focus
-                    split-window-right
-                    split-window-right-and-focus))
-            (defalias 'evil-visual-update-x-selection 'ignore)
+  :init
+  (progn
+    (setq evil-want-Y-yank-to-eol t)
+    (setq evil-symbol-word-search t)
+    (setq evil-want-C-u-scroll t)
+    (evil-mode))
+  :config
+  (progn
+    (mapc #'evil-declare-ignore-repeat
+          '(kill-this-buffer
+            ido-kill-buffer
+            outline-next-visible-heading
+            outline-previous-visible-heading
+            outline-up-heading
+            evil-visualstar/begin-search-forward
+            evil-visualstar/begin-search-backward
+            org-export-dispatch
+            org-end-of-line
+            org-beginning-of-line
+            org-open-at-point
+            org-cycle
+            org-shifttab
+            org-ctrl-c-ctrl-c
+            org-next-visible-heading
+            org-previous-visible-heading
+            split-window-below
+            split-window-below-and-focus
+            split-window-right
+            split-window-right-and-focus))
+    (defalias 'evil-visual-update-x-selection 'ignore)
 
-            (setq evil-normal-state-cursor '(box "DarkGoldenrod2")
-                  evil-insert-state-cursor '((bar . 2) "chartreuse3")
-                  evil-emacs-state-cursor '(box "SkyBlue2")
-                  evil-replace-state-cursor '((hbar . 2) "chocolate")
-                  evil-visual-state-cursor '((hbar . 2) "gray")
-                  evil-motion-state-cursor '(box "plum3"))
+    (setq evil-normal-state-cursor '(box "DarkGoldenrod2")
+          evil-insert-state-cursor '((bar . 2) "chartreuse3")
+          evil-emacs-state-cursor '(box "SkyBlue2")
+          evil-replace-state-cursor '((hbar . 2) "chocolate")
+          evil-visual-state-cursor '((hbar . 2) "gray")
+          evil-motion-state-cursor '(box "plum3"))
 
-            (setq-default evil-shift-width 2)
+    (setq-default evil-shift-width 2)
 
-            (evil-define-motion evil-goto-line (count)
-              "Go to the first non-blank character of line COUNT.
+    (evil-define-motion evil-goto-line (count)
+      "Go to the first non-blank character of line COUNT.
 By default the (truly) last line."
-              :jump t
-              :type line
-              (if (null count)
-                  (goto-char (buffer-size))
-                (goto-char (point-min))
-                (forward-line (1- count)))
-              (evil-first-non-blank))
+      :jump t
+      :type line
+      (if (null count)
+          (goto-char (buffer-size))
+        (goto-char (point-min))
+        (forward-line (1- count)))
+      (evil-first-non-blank))
 
-            ;; esc should always quit: http://stackoverflow.com/a/10166400/61435
-            (define-key evil-normal-state-map [escape] 'keyboard-quit)
-            (define-key evil-visual-state-map [escape] 'keyboard-quit)
-            (define-key minibuffer-local-map [escape] 'abort-recursive-edit)
-            (define-key minibuffer-local-ns-map [escape] 'abort-recursive-edit)
-            (define-key minibuffer-local-completion-map [escape] 'abort-recursive-edit)
-            (define-key minibuffer-local-must-match-map [escape] 'abort-recursive-edit)
-            (define-key minibuffer-local-isearch-map [escape] 'abort-recursive-edit)
-            ;; Escape from isearch-mode("/" and "?" in evil-mode) like vim
-            (define-key isearch-mode-map (kbd "<escape>") 'isearch-cancel)
-            (define-key evil-motion-state-map [C-i] 'evil-jump-forward)))
+    ;; esc should always quit: http://stackoverflow.com/a/10166400/61435
+    (define-key evil-normal-state-map [escape] 'keyboard-quit)
+    (define-key evil-visual-state-map [escape] 'keyboard-quit)
+    (define-key minibuffer-local-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-ns-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-completion-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-must-match-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-isearch-map [escape] 'abort-recursive-edit)
+    ;; Escape from isearch-mode("/" and "?" in evil-mode) like vim
+    (define-key isearch-mode-map (kbd "<escape>") 'isearch-cancel)
+    (define-key evil-motion-state-map [C-i] 'evil-jump-forward)))
 
 (use-package general
-  :config (progn
-            (general-create-definer
-             private/set-leader-keys
-             :prefix "SPC"
-             :non-normal-prefix "M-m"
-             :keymaps '(motion insert emacs))
-            (general-create-definer
-             private/set-leader-keys-for-mode
-             :prefix "'"
-             :non-normal-prefix "M-m m"
-             :states '(motion insert emacs))
-            (private/set-leader-keys
-             "" nil
+  :config
+  (progn
+    (general-create-definer
+     private/set-leader-keys
+     :prefix "SPC"
+     :non-normal-prefix "M-m"
+     :keymaps '(motion insert emacs))
+    (general-create-definer
+     private/set-leader-keys-for-mode
+     :prefix "'"
+     :non-normal-prefix "M-m m"
+     :states '(motion insert emacs))
+    (private/set-leader-keys
+     "" nil
 
-             "SPC" 'execute-extended-command
+     "SPC" 'execute-extended-command
 
-             "u" 'universal-argument
+     "u" 'universal-argument
 
-             ;; File
-             "ff" 'find-file
-             "fel" 'find-library
-             "fS" 'evil-write-all
-             "fs" 'save-buffer
-             "fvd" 'add-dir-local-variable
-             "fvf" 'add-file-local-variable
-             "fvp" 'add-file-local-variable-prop-line
+     ;; File
+     "ff" 'find-file
+     "fel" 'find-library
+     "fS" 'evil-write-all
+     "fs" 'save-buffer
+     "fvd" 'add-dir-local-variable
+     "fvf" 'add-file-local-variable
+     "fvp" 'add-file-local-variable-prop-line
 
-             ;; Buffer
-             "bb" 'switch-to-buffer
-             "bd" 'kill-this-buffer
-             "bw" 'read-only-mode
-             "TAB" (lambda () (interactive) (switch-to-buffer nil))
+     ;; Buffer
+     "bb" 'switch-to-buffer
+     "bd" 'kill-this-buffer
+     "bw" 'read-only-mode
+     "TAB" (lambda () (interactive) (switch-to-buffer nil))
 
-             ;; Help
-             "sj" 'imenu
+     ;; Help
+     "sj" 'imenu
 
-             ;; Jumping
-             "hdf" 'describe-function
-             "hdk" 'describe-key
-             "hdv" 'describe-variable
+     ;; Jumping
+     "hdf" 'describe-function
+     "hdk" 'describe-key
+     "hdv" 'describe-variable
 
-             ;; Themes
-             "Ts"  'load-theme
+     ;; Themes
+     "Ts"  'load-theme
 
-             ;; Window Management
-             "wd" 'delete-window
-             "wm" 'delete-other-windows
-             "wf" 'make-frame
-             "wH" 'evil-window-move-far-left
-             "wh" 'evil-window-left
-             "wJ" 'evil-window-move-very-bottom
-             "wj" 'evil-window-down
-             "wK" 'evil-window-move-very-top
-             "wk" 'evil-window-up
-             "wL" 'evil-window-move-far-right
-             "wl" 'evil-window-right
-             "wo" 'other-frame
-             "ws" 'split-window-below
-             "wS" 'split-window-below-and-focus
-             "wv" 'split-window-right
-             "wV" 'split-window-right-and-focus
-             "ww" 'other-window
-             "w=" 'balance-windows
+     ;; Window Management
+     "wd" 'delete-window
+     "wm" 'delete-other-windows
+     "wf" 'make-frame
+     "wH" 'evil-window-move-far-left
+     "wh" 'evil-window-left
+     "wJ" 'evil-window-move-very-bottom
+     "wj" 'evil-window-down
+     "wK" 'evil-window-move-very-top
+     "wk" 'evil-window-up
+     "wL" 'evil-window-move-far-right
+     "wl" 'evil-window-right
+     "wo" 'other-frame
+     "ws" 'split-window-below
+     "wS" 'split-window-below-and-focus
+     "wv" 'split-window-right
+     "wV" 'split-window-right-and-focus
+     "ww" 'other-window
+     "w=" 'balance-windows
 
-             "qf" 'delete-frame
-             "qq" 'kill-emacs)
-            (general-define-key
-             :status 'motion
-             :keymaps '(info-mode-map compilation-mode-map)
-             "SPC" nil)
-            (general-define-key
-             :keymaps 'input-decode-map
-             "C-h" "DEL"
-             "M-h" "C-h"
-             "C-i" [C-i])))
+     "qf" 'delete-frame
+     "qq" 'kill-emacs)
+    (general-define-key
+     :status 'motion
+     :keymaps '(info-mode-map compilation-mode-map)
+     "SPC" nil)
+    (general-define-key
+     :keymaps 'input-decode-map
+     "C-h" "DEL"
+     "M-h" "C-h"
+     "C-i" [C-i])))
 
 (use-package evil-nerd-commenter
   :defer t
