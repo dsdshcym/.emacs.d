3b02991fecf45cff45025c8caafeb7bbfde8338e
Yiming Chen <dsdshcym@gmail.com>
2018-02-28 15:23:03 +0800

    Rename emacs.org to config.org
    - I already have an org file named emacs.org

--- init.el
+++ init.el
@@ -9,10 +9,10 @@
 ;; Don't attempt to find/apply special file handlers to files loaded during startup.
 (let ((file-name-handler-alist nil))
   ;; If config is pre-compiled, then load that
-  (if (file-exists-p (expand-file-name "emacs.elc" user-emacs-directory))
-      (load-file (expand-file-name "emacs.elc" user-emacs-directory))
+  (if (file-exists-p (expand-file-name "config.elc" user-emacs-directory))
+      (load-file (expand-file-name "config.elc" user-emacs-directory))
     ;; Otherwise use org-babel to tangle and load the configuration
     (require 'org)
-    (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))))
+    (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
 
 ;;; init.el ends here
